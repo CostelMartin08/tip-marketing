897cc04c3600b31488d32a913066ef49187b5d3b
Costel <costelmartinescu2000@gmail.com>
2024-07-01 18:37:14 +0300

    organization & check form data

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,12 +6,12 @@ import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import ScrollTrigger from 'gsap/dist/ScrollTrigger';
 import { Key, useEffect, useRef } from "react";
 
-import Header from '../sections/header';
-import SectionText from '../components/sectionText';
+import Header from '../sections/Header';
+import SectionText from '../components/SectionText';
 import Offer from '../sections/Offer';
 import Projects from '../sections/Projects';
 import Contact from '../sections/Contact';
-import Footer from '../sections/footer';
+import Footer from '../sections/Footer';
 
 import { offerData, sectionData, projectData } from '../data/data';
 
